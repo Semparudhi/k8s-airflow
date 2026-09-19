487c722a31b3e20f09a5a5d93cd0a067c0fa0387
sem <semparudhi007@gmail.com>
2026-09-19 12:19:49 +0000

    adding waiting time to minio spark job

--- minio_probe.py
+++ minio_probe.py
@@ -1,7 +1,10 @@
+import time
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("minio-probe").getOrCreate()
 df = spark.read.option("header", True).csv("s3a://spark-data/input/")
+print("SLEEPING")
+time.sleep(300)
 df.write.mode("overwrite").partitionBy("city").parquet("s3a://spark-data/output/by_city/")
 print("ROWS WRITTEN:", df.count())
 spark.stop()
